fb9bc14f7b0fcf2c1943db66a70d1915104f2aa9
Almudena Rendón Fernández <almudena.rendon@atradius.com>
2026-02-24 20:08:50 +0100

    Version 1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      outDir: 'dist',
+      insertTypesEntry: true,
+    })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
